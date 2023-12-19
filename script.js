2a6f71d6288e49aba2f20462926a200c64718283
Leon Sjöholm <leon.sjoholm@elev.ga.ntig.se>
2023-12-19 14:04:15 +0100

    miniräknare klar

---
+++ script.js
@@ -0,0 +1,26 @@
+let tal1_elem = document.querySelector("#tal1");
+let tal2_elem = document.querySelector("#tal2");
+let svar_elem = document.querySelector("#answer");
+function plus(){
+    let tal1 = parseInt(tal1_elem.value);
+    let tal2 = parseInt(tal2_elem.value);
+    svar_elem.value=(tal1+tal2);
+}
+function minus(){
+    let tal1 = parseInt(tal1_elem.value);
+    let tal2 = parseInt(tal2_elem.value);
+    svar_elem.value=(tal1-tal2);
+
+
+}
+function multiply(){
+    let tal1 = parseInt(tal1_elem.value);
+    let tal2 = parseInt(tal2_elem.value);
+    svar_elem.value=(tal1*tal2);
+    
+}
+function divide(){
+    let tal1 = parseInt(tal1_elem.value);
+    let tal2 = parseInt(tal2_elem.value);
+    svar_elem.value=(tal1/tal2);
+}
